e2807958d97daa20b4b71842ce0a0cb18907acd9
Jeff Newbern <jnewbern@yahoo.com>
2009-02-08 23:27:10 -0500

    Add code for an environment monad and transformer and use it to add an environment of variable bindings to the interpreter example.

--- clojure/interp.clj
+++ clojure/interp.clj
@@ -1,6 +1,37 @@
 (ns monad-tutorial
   (:use clojure.contrib.monads))
 
+; environment monad
+
+(defmonad env
+  "Monad which allows a computation to access
+   values from an environment"
+  [m-result (fn m-result-env [v]
+	      (fn [_] v))
+   m-bind   (fn m-bind-env [mv f]
+	      (fn [e] ((f (mv e)) e)))
+   ])
+
+(defn ask-env [k]
+  (fn [e] (second (find e k))))
+
+(defn local-env [f mv]
+  (fn [e] (mv (f e))))
+
+(defn run-with-env [e mv]
+  (mv e))
+
+(defn env-t
+  "Monad transformer that adds an environment to an existing monad"
+  [m]
+  (monad [m-result (with-monad m
+		     (fn m-result-env-t [v]
+		       (fn [_] (m-result v))))
+	  m-bind   (with-monad m
+		     (fn m-bind-env-t [mv f]
+		       (fn [e] (m-bind (run-with-env e mv)
+				       (fn [x] ((f x) e))))))]))
+
 ; arithmetic language fragment
 
 (defstruct expr :tag :val)
@@ -11,36 +42,55 @@
 (defn times [x y] (struct expr 'times [x y]))
 (defn divide [x y] (struct expr 'divide [x y]))
 
+; variable references
+
+(defn variable [ident] (struct expr 'var ident))
+
+; interpreter
+
+(def interp-monad (env-t maybe))
+
 (defn interp [e]
   (let [t (:tag e)]
     (cond
-       (= t 'literal) (domonad maybe
+       (= t 'literal) (domonad interp-monad
 			 []
 			 (:val e))
-       (= t 'plus)    (domonad maybe
+       (= t 'plus)    (domonad interp-monad
 			 [x (interp (first (:val e)))
 			  y (interp (second (:val e)))]
 			 (+ x y))
-       (= t 'minus)   (domonad maybe
+       (= t 'minus)   (domonad interp-monad
 			 [x (interp (first (:val e)))
 			  y (interp (second (:val e)))]
 			 (- x y))
-       (= t 'times)   (domonad maybe
+       (= t 'times)   (domonad interp-monad
 			 [x (interp (first (:val e)))
 			  y (interp (second (:val e)))]
 			 (* x y))
-       (= t 'divide)  (domonad maybe
+       (= t 'divide)  (domonad interp-monad
 			 [x (interp (first (:val e)))
 			  y (interp (second (:val e)))
 			  :when (not (= y 0))]
-			 (/ x y)))))
-
+			 (/ x y))
+       (= t 'var)     (domonad interp-monad
+			 [v (ask-env (:val e))
+			  r (interp v)]
+                         r)
+       )))
 
 ; examples
 
-; (prn (interp (plus (literal 3) (times (literal 2) (literal 7)))))
+(def initial-env {"x" (literal 7), "y" (literal 21)})
 
-; (prn (interp (divide (literal 12) (literal 3))))
+(prn (run-with-env initial-env
+		   (interp (plus (literal 3) (times (literal 2) (variable "x"))))))
 
-; (prn (interp (divide (literal 12) (minus (literal 3) (literal 3)))))
+(prn (run-with-env initial-env
+		   (interp (plus (literal 4) (variable "z")))))
 
+(prn (run-with-env initial-env
+		   (interp (divide (literal 12) (literal 3)))))
+
+(prn (run-with-env initial-env
+		   (interp (divide (literal 12) (minus (literal 21) (variable "y"))))))
