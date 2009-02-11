f42c7710f5aa931f6c33d50e93ae3eeb1d3c06e9
Jeff Newbern <jnewbern@yahoo.com>
2009-02-11 10:41:57 -0500

    Add error monad to provide more informative failure information.

--- clojure/interp.clj
+++ clojure/interp.clj
@@ -1,6 +1,26 @@
 (ns monad-tutorial
   (:use clojure.contrib.monads))
 
+; error monad
+
+(defmonad error
+  "Monad describing computations with possible failures.
+   Values in the monad pair a status code with either a value
+   or a failure descriptor."
+  [m-result  (fn m-result-error [v]  (list 'ok v))
+   m-bind    (fn m-bind-error [mv f] (if (= 'ok (first mv))
+				         (f (second mv))
+					 mv))
+   ])
+
+(defn fail [err] (list 'fail err))
+
+(defn success? [mv] (= 'ok (first mv)))
+(defn failure? [mv] (not (success? mv)))
+
+(defn successful-value [mv] (if (success? mv) (second mv) 'nil))
+(defn error-desc       [mv] (if (failure? mv) (second mv) 'nil))
+
 ; environment monad
 
 (defmonad env
@@ -11,6 +31,8 @@
    m-bind   (fn m-bind-env [mv f]
 	      (fn [e] ((f (mv e)) e)))
    ])
+
+(defn capture-env [e] e)
 
 (defn ask-env [k]
   (fn [e] (second (find e k))))
@@ -32,21 +54,29 @@
 		       (fn [e] (m-bind (run-with-env e mv)
 				       (fn [x] ((f x) e))))))]))
 
+(defn capture-env-t [m]
+  (fn [e] (with-monad m (m-result e))))
 
-(defn capture-env [e] e)
+(defn ask-env-t [m k]
+  (domonad (env-t m) [e (capture-env-t m)] (second (find e k))))
 
 ; interpreter
 
-(def interp-monad (env-t maybe))
+(def interp-monad (env-t error))
 
-(defn add-to-map [k v]
-  (fn [m] (assoc m k v)))
+(defn report-error [desc]
+  (with-monad env (m-result (fail desc))))
+
+(defn add-to-env [e k v]
+  (with-monad error (assoc e k (m-result v))))
 
 (defn interp [e]
   (cond
    (symbol? e) (domonad interp-monad
-		  [v (ask-env e)
-		   r (interp v)]
+		  [v (ask-env-t error e)
+		   r (if v
+		         (m-bind (with-monad env (m-result v)) interp)
+			 (report-error (str "undefined variable " e)))]
 		  r)
    (number? e) (domonad interp-monad [] e)
    (seq? e)    (let [t    (first e)
@@ -67,27 +97,29 @@
 		  (= t '/)        (domonad interp-monad
 				     [x (interp (first args))
 				      y (interp (second args))
-				      :when (not (and (number? y) (= 0 y)))]
-				     (/ x y))
+				      r (if (and (number? y) (not= 0 y))
+					    (m-result (/ x y))
+					    (report-error "division by 0"))]
+				     r)
 		  (= t 'closure)  (domonad interp-monad
-				     []
-				     (run-with-env (first args) (interp (second args))))
+				     [r (local-env (fn [_] (first args)) (interp (second args)))]
+				     r)
 		  (= t 'lambda-v) (domonad interp-monad
-				     [ce capture-env]
+				     [ce (capture-env-t error)]
 				     (fn [arg_cl] (domonad interp-monad
 						     [arg_val (interp arg_cl)
-						      body_cl (m-result (list 'closure (assoc ce (first args) arg_val) (second args)))
+						      body_cl (m-result (list 'closure (add-to-env ce (first args) arg_val) (second args)))
 						      r       (interp body_cl)]
 						     r)))
 		  (= t 'lambda-n) (domonad interp-monad
-				     [ce capture-env]
+				     [ce (capture-env-t error)]
 				     (fn [arg_cl] (domonad interp-monad
-						     [body_cl (m-result (list 'closure (assoc ce (first args) arg_cl) (second args)))
+						     [body_cl (m-result (list 'closure (add-to-env ce (first args) arg_cl) (second args)))
 						      r       (interp body_cl)]
 						     r)))
 		  (= t 'app)      (domonad interp-monad
 				     [f  (interp (first args))
-				      ce capture-env
+				      ce (capture-env-t error)
 				      r  (f (list 'closure ce (second args)))]
 				     r)
 		  ))
@@ -95,13 +127,13 @@
 
 ; examples
 
-(def initial-env {'x 7, 'y 21})
+(def initial-env (with-monad error {'x (m-result 7), 'y (m-result 21)}))
 
 ; Success: 17
 (prn (run-with-env initial-env
 		   (interp '(+ 3 (* 2 x)))))
 
-; Error: undefined variable "z"
+; Error: undefined variable: z
 (prn (run-with-env initial-env
 		   (interp '(+ 4 z))))
 
