66d7177c25e68c3aaa036392513cfacf93fe9c29
Ravi Nanavati <ravi@bluespec.com>
2009-03-21 23:19:31 -0400

    Add monadic interpreter exercise solution Move log-lang from interp.clj to exercise solution

--- clojure/interp.clj
+++ clojure/interp.clj
@@ -709,85 +709,6 @@
 	   '(ok 6))
   )
 
-; a language fragment that accumulates a log of numbers in a string
-; we also provide the ability to print them
-(defn write-number [m v]
-  (with-monad m
-    (if (number? v)
-      (domonad
-       [_ (m-write (str v))
-	_ (m-write " ")]
-       nil)
-      (m-fail (str "attempt to write non-number: " v)))))
-
-; our log is just a string
-(def empty-log clojure.contrib.accumulators/empty-string)
-
-(def log-lang
-  [ ; log a number
-   [ (fn [e]
-       (and (seq? e) (= (first e) 'write-nums)))
-     (fn [m rec e]
-       (domonad m
-	 [args (m-result (rest e))
-	  vals (m-map rec args)
-          _    (m-seq (map (partial write-number m) vals))]
-	 nil)) ]
-   ,
-   ; collect and print the numbers logged while evaluating
-   ; the list of argument expressions
-   ; return the last expression as the result
-   [ (fn [e] (and (seq? e) (= (first e) 'collect-nums)))
-     (fn [m rec e]
-       (domonad m
-         [args (m-result (rest e))
-	  [vals log] (m-listen (m-map rec args))
-	  _ (m-result (println (str "number log: " log)))
-	  ]
-	 (last vals))) ]
-   ,
-   ; censor any values logged while evaluating its argument
-   [ (fn [e] (and (seq? e) (= (first e) 'censor-nums)))
-     (fn [m rec e]
-       (with-monad m
-	 (m-censor (constantly empty-log) (rec (second e)))))
-   ]
-  ])
-
-(def arith-log-interp
-  (let [ monad     (writer-t empty-log error-m)
-	 parts     [ arith-lang, log-lang, do-lang ]
-	 otherwise fail-bad-token
-	 interp    (make-interp monad parts otherwise)
-       ]
-    ; we don't eval-writer-t because we want to see the final log
-    (fn [e] (interp e))))
-
-(defn do-arith-log-tests []
-  (let [test (partial do-test arith-log-interp)]
-    (test "log-one" '(write-nums 5) '(ok [nil "5 "]))
-    (test "log-two" '(do (write-nums 16) (write-nums (+ 5 7)))
-	  '(ok [nil "16 12 "]))
-    (test "censor-one" '(censor-nums (write-nums 19)) '(ok [nil ""]))
-    (test "censor-two"
-	  '(do (censor-nums (write-nums (+ 7 9)))
-	       (write-nums (* 8 12)))
-	  '(ok [nil "96 "]))
-    (println "\nlisten-one should print: " "number log: 17 23 ")
-    (test "listen-one"
-	  '(censor-nums
-	    (collect-nums
-	     (write-nums 17 23)))
-	  '(ok [nil ""]))
-    (println "\nlisten-two should print the number log and return it")
-    (test "listen-two"
-	  '(collect-nums
-	    (write-nums (+ 17 (do (write-nums 12) 23)))
-	    (censor-nums (do (write-nums 99) 41)))
-	  '(ok [41 "12 40 "]))
-
-  ))
-
 (defn do-tests []
   (do-arith-tests)
   (do-arith-env-tests)
@@ -796,7 +717,6 @@
   (do-interp-with-time-tests)
   (do-ref-do-fn-cont-tests)
   (do-interp-with-time-ref-tests)
-  (do-arith-log-tests)
   )
 
   
